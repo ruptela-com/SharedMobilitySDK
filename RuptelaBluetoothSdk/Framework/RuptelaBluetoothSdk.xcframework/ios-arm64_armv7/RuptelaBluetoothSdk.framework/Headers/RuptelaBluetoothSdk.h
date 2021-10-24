f7703fd0d553793f399ffbbdb828dd88f482074a
Aurimas Petrevicius <aurimas@notnot.xyz>
2021-10-24 20:46:17 +0300

    replace framework with xcframework

---
+++ RuptelaBluetoothSdk/Framework/RuptelaBluetoothSdk.xcframework/ios-arm64_armv7/RuptelaBluetoothSdk.framework/Headers/RuptelaBluetoothSdk.h
@@ -0,0 +1,10 @@
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RuptelaBluetoothSdk.
+FOUNDATION_EXPORT double RuptelaBluetoothSdkVersionNumber;
+
+//! Project version string for RuptelaBluetoothSdk.
+FOUNDATION_EXPORT const unsigned char RuptelaBluetoothSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RuptelaBluetoothSdk/PublicHeader.h>
